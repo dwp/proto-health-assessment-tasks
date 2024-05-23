4a0d4870e3db3d14d4a4bc34f2b320ebc42a1ee0
James Ashcroft <49193997+jamesashcroft@users.noreply.github.com>
2024-05-23 16:04:26 +0100

    Merge pull request #113 from dwp/SREL
    Date on one field in SREL
    
    GitOrigin-RevId: d75800c39ec4be821bb5ad7d001f0169b4ecde87

--- app/filters.js
+++ app/filters.js
@@ -1,2 +1,15 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const addFilter = govukPrototypeKit.views.addFilter
+
+addFilter('monthName', function () {
+    const monthName = {
+        month: {
+          1: 'January',
+          2: 'Feburary',
+          3: 'March',
+          4: 'April',
+        }
+      }
+           
+  return monthName
+})
