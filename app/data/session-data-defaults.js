293a7c66d83fb58e9466538648b9c354b5d1704c
James Ashcroft <james.ashcroft@engineering.digital.dwp.gov.uk>
2024-06-11 11:46:29 +0100

    Pages for examinations

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -3,6 +3,20 @@ module.exports = {
   // Insert values here
 
   'shoulderNotExamined': 'None',
-  'elbowNotExamined': 'None'
+  'elbowNotExamined': 'None',
+  'wristNotExamined': 'None',
+  'handsNotExamined': 'None',
+  'thumbNotExamined': 'None',
+  'gripNotExamined': 'None',
+  'cervicalSpineNotExamined': 'None',
+  'lumbarSpineNotExamined': 'None',
+  'hipNotExamined': 'None',
+  'kneeNotExamined': 'None',
+  'ankleNotExamined': 'None',
+  'halluxNotExamined': 'None',
+  'cardio-respriatoryNotExamined': 'None',
+  'visionNotExamined': 'None',
+  'hearingNotExamined': 'None',
+  'neurologicalNotExamined': 'None'
 
 }
