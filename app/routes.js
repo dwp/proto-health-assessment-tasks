b4d44e2c0b1a1860a4bad761b0362523ce455b40
James Ashcroft <49193997+jamesashcroft@users.noreply.github.com>
2024-12-09 08:37:30 +0000

    Merge pull request #231 from dwp/validation
    Updated medications
    
    GitOrigin-RevId: 6663e1da2131822f83cad22ad9abbcff9a5ff793

--- app/routes.js
+++ app/routes.js
@@ -3935,6 +3935,23 @@ router.post('/validation-omissions/medication-change-error', function(req, res) 
 res.redirect('/validation-omissions/report-error')
 })
 
+router.post('/validation-omissions/medication-single-error', function(req, res) {
+  const medicationName = req.session.data['medications-name']
+  const medicationDose = req.session.data['medications-dose']
+  const medicationFrequency = req.session.data['medications-frequency']
+  const medicationReason = req.session.data['medications-reason']
+  const medicationEffectiveness = req.session.data['medications-efficacy']
+  const medicationSideEffect = req.session.data['medications-side-effects']
+  const medNo = req.session.data['indexMed']
+
+  const medicationAdded = req.session.data.medicationAdded || []
+    medicationAdded.splice(medNo, 1);
+    medicationAdded.push({ medNo, medicationName, medicationDose, medicationFrequency, medicationReason, medicationEffectiveness, medicationSideEffect })
+    req.session.data.medicationAdded = medicationAdded
+
+res.redirect('/validation-omissions/medication-single?source=Medication')
+})
+
 router.post('/validation-omissions/medication-view', function(req, res) {
 
   res.redirect('/validation-omissions/medication-single')
