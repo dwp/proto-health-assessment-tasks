1e6bb7a30cf289e6eea2883aff035375c1ba80d3
James Ashcroft <49193997+jamesashcroft@users.noreply.github.com>
2025-03-26 10:56:40 +0000

    Merge pull request #297 from dwp/JA-evidence
    Updates to iterated journey
    
    GitOrigin-RevId: 244e1ed730cb8e8b2d816ea20b91b500314aaf5f

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -24,6 +24,7 @@ module.exports = {
   'relationshipToClaimant': 'None',
   'document-name': 'None',
   'dwp-evidence': 'None',
-  'evidenceDeclaration': 'None'
+  'evidenceDeclaration': 'None',
+  'harmfulInfoBox': 'None'
 
 }
